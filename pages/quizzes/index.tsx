1e36aef49cfadfa392839f18248a101c68a53247
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-01-13 12:10:15 +0900

    :sparkles: feat: クイズの削除機能の追加

--- pages/quizzes/index.tsx
+++ pages/quizzes/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { useState, useEffect, FC, Fragment } from 'react';
 import supabase from '../../utils/supabase';
-import { atom, useAtom } from 'jotai';
+import { useAtom } from 'jotai';
 import { messageForQuizCrudAtom } from '../../features/quizzes/store';
 
 type Quiz = {
@@ -9,10 +9,14 @@ type Quiz = {
   commentary: string;
   created_at: string;
 };
+const MESSAGE: { [key: string]: { [key: string]: string } } = {
+  DELETE: { CONFIRM: '本当に削除しますか？', SUCCESS: '削除しました。' },
+};
 
 const Index: FC = () => {
-  const [message] = useAtom<string>(messageForQuizCrudAtom);
+  const [message, setMessage] = useAtom(messageForQuizCrudAtom);
   const [quizzes, setQuizzes] = useState<Quiz[]>([]);
+
   const fetchQuizzes = async (): Promise<void> => {
     const { data } = await supabase
       .from('quizzes')
@@ -25,6 +29,18 @@ const Index: FC = () => {
   useEffect(() => {
     fetchQuizzes();
   }, []);
+
+  const deleteQuizBy = async (id: number): Promise<void> => {
+    if (!window.confirm(MESSAGE.DELETE.CONFIRM)) return;
+    try {
+      await supabase.from('quizzes').delete().eq('id', id);
+      fetchQuizzes();
+      setMessage(MESSAGE.DELETE.SUCCESS);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <div className="text-3xl font-bold underline">Index</div>
@@ -32,8 +48,17 @@ const Index: FC = () => {
       <p className="text-rose-600">{message}</p>
       {quizzes.map<JSX.Element>((quiz: Quiz) => (
         <Fragment key={quiz.id}>
+          <Link
+            className="text-blue-600 hover:text-blue-700 mr-2 hover:underline"
+            href={`quizzes/${quiz.id}/edit`}>
+            編集
+          </Link>
+          <button
+            className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 mr-2 rounded"
+            onClick={() => deleteQuizBy(quiz.id)}>
+            削除
+          </button>
           {quiz.commentary}
-          <Link href={`quizzes/${quiz.id}/edit`}>編集</Link>
           <br></br>
         </Fragment>
       ))}
