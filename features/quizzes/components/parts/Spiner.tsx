cc9fe0ec17301a845d5dc126ed228aaec075994c
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-01-18 19:14:55 +0900

    :sparkles: Spinerを追加する

---
+++ features/quizzes/components/parts/Spiner.tsx
@@ -0,0 +1,9 @@
+export const Spiner = () => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="animate-ping h-10 w-10 bg-blue-600 rounded-full"></div>
+      </div>
+    </>
+  );
+};
