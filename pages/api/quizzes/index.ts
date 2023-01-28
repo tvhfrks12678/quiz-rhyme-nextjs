2710bb7eba6d6aea22ccdd8f81b2c0c167ef0d93
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-01-29 07:45:17 +0900

    :sparkles: クイズの登録画面にYouTube埋め込み用の入力欄を追加する

--- pages/api/quizzes/index.ts
+++ pages/api/quizzes/index.ts
@@ -1,26 +1,27 @@
-import { Quiz } from '@prisma/client';
-import prisma from 'lib/prisma';
-import { NextApiRequest, NextApiResponse } from 'next';
+import { Quiz } from '@prisma/client'
+import prisma from 'lib/prisma'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 const handle = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void | NextApiResponse<Quiz>> => {
-  const { userId, commentary } = req.body;
+  const { userId, commentary, youtubeEmbed } = req.body
 
   try {
     const result = await prisma.quiz.create({
       data: {
         userId: userId,
         commentary: commentary,
+        youtubeEmbed: youtubeEmbed,
       },
-    });
-    console.log(result);
-    return res.status(201).json(result);
+    })
+    console.info(result)
+    return res.status(201).json(result)
   } catch (error) {
-    console.log(error);
-    return res.status(500).end(error);
+    console.error(error)
+    return res.status(500).end(error)
   }
-};
+}
 
-export default handle;
+export default handle
--- pages/quizzes/new.tsx
+++ pages/quizzes/new.tsx
@@ -1,31 +1,33 @@
-import { Nav } from 'features/quizzes/components/Nav';
-import { Headline } from 'features/quizzes/components/parts/Headline';
-import { messageForQuizCrudAtom } from 'features/quizzes/store';
-import { useSetAtom } from 'jotai';
-import Router from 'next/router';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import supabase from 'utils/supabase';
+import { Nav } from 'features/quizzes/components/Nav'
+import { Headline } from 'features/quizzes/components/parts/Headline'
+import { messageForQuizCrudAtom } from 'features/quizzes/store'
+import { useSetAtom } from 'jotai'
+import Router from 'next/router'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import supabase from 'utils/supabase'
+import toast, { Toaster } from 'react-hot-toast'
 
 type Inputs = {
-  commentary: string;
-};
+  commentary: string
+  youtubeEmbed: string
+}
 
-const MESSAGE_RESISTER_SUCCESS = 'クイズを登録しました。';
-const HEADLINE_WORD = 'クイズ新規登録';
+const MESSAGE_RESISTER_SUCCESS = 'クイズを登録しました。'
+const HEADLINE_WORD = 'クイズ新規登録'
 
 const New = () => {
-  const setMessage = useSetAtom(messageForQuizCrudAtom);
-  const { register, handleSubmit } = useForm<Inputs>();
+  const setMessage = useSetAtom(messageForQuizCrudAtom)
+  const { register, handleSubmit } = useForm<Inputs>()
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    console.log(data);
+    console.log(data)
 
     const {
       data: { user },
-    } = await supabase.auth.getUser();
+    } = await supabase.auth.getUser()
 
     if (!user) {
-      Router.push({ pathname: '/login' });
-      return;
+      Router.push({ pathname: '/login' })
+      return
     }
 
     try {
@@ -37,30 +39,50 @@ const New = () => {
         body: JSON.stringify({
           userId: user.id,
           commentary: data.commentary,
+          youtubeEmbed: data.youtubeEmbed,
         }),
-      });
-      console.info(response);
-      const createdData = await response.json();
-      console.info(createdData);
+      })
+      console.info(response)
+      const createdData = await response.json()
+      console.info(createdData)
       if (response.ok) {
-        setMessage(MESSAGE_RESISTER_SUCCESS);
-        Router.push({ pathname: '/quizzes' });
-        return;
+        setMessage(MESSAGE_RESISTER_SUCCESS)
+        Router.push({ pathname: '/quizzes' })
+        return
       }
     } catch (error) {
-      console.error(error);
+      toast.error('クイズの登録に失敗しました')
+      console.error(error)
     }
-  };
+  }
   return (
     <>
       <Nav />
       <Headline headlineWord={HEADLINE_WORD} />
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input {...register('commentary')} />
+        <div className="form-control mb-4">
+          <label className="input-group input-group-vertical">
+            <span>Commentary</span>
+            <textarea
+              {...register('commentary')}
+              placeholder="例: 韻が2つある"
+              className="input input-bordered textarea"></textarea>
+          </label>
+        </div>
+        <div className="form-control mb-4">
+          <label className="input-group input-group-vertical">
+            <span>YouTube Embed</span>
+            <textarea
+              {...register('youtubeEmbed')}
+              placeholder="例: https://youtube.com/embed/eBLdt5emyqk?clip=UgkxxIYOSXUeagWeNxgGg4LLDATxzgGAf3Zs&amp;clipt=ELD5CRiwxQo"
+              className="input input-bordered textarea"></textarea>
+          </label>
+        </div>
         <button type="submit">登録</button>
       </form>
+      <Toaster />
     </>
-  );
-};
+  )
+}
 
-export default New;
+export default New
