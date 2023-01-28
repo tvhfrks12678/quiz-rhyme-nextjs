5e9fe80bd7828e55b79480eab1d60d67d9ad48f9
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-01-28 12:27:41 +0900

    :sparkles: model: Rhymeテーブルを追加する

---
+++ prisma/migrations/20230128032237_create_rhyme/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Rhyme" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Rhyme_pkey" PRIMARY KEY ("id")
+);
