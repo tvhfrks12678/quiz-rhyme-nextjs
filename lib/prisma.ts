0458eee76e86b963bc4c3441191cf7a7b9aafa20
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-02-02 15:45:01 +0900

    :loud_sound: prisma: SQL確認用のログを追加する

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,36 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client'
 
-let prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+})
 
-export default prisma;
+const CONTROL_CHARACTER_COLOR = {
+  BLUE: '\u001b[34m',
+  RESET: '\u001b[0m',
+}
+
+prisma.$on('query', (e: Prisma.QueryEvent) => {
+  console.log(
+    `${CONTROL_CHARACTER_COLOR.BLUE}'prisma:query'${CONTROL_CHARACTER_COLOR.RESET}`
+  )
+  console.info(e)
+})
+
+export default prisma
