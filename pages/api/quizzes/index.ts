9572434e1662db7c20cf8050ddeac002b226728b
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-02-06 08:50:59 +0900

    :sparkles: back: クイズの表示処理を追加する

--- pages/api/quizzes/index.ts
+++ pages/api/quizzes/index.ts
@@ -1,8 +1,9 @@
 import { Prisma, Quiz } from '@prisma/client'
+import { QuizToPlay } from 'features/quizPlay/types/quizToPlay'
 import prisma from 'lib/prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-type Choice = {
+type InputChoice = {
   content: string
   rhyme: string
 }
@@ -11,72 +12,97 @@ const handle = async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void | NextApiResponse<Quiz>> => {
-  const { userId, commentary, youtubeEmbed, choices } = req.body
-  try {
-    await prisma.$transaction(
-      async (tx: Prisma.TransactionClient) => {
-        const rhymeWords: string[] = choices
-          .map((choice: Choice) => {
-            if (choice.rhyme) return choice.rhyme
-          })
-          .filter(Boolean)
-
-        let rhymesKeyContentValueId: { [key: string]: string } = {}
-
-        for (const rhymeWord of rhymeWords) {
-          const rhyme = await tx.rhyme.findUnique({
-            where: {
-              content: rhymeWord,
+  if (req.method === 'GET') {
+    try {
+      const quiz: QuizToPlay[] = await prisma.quiz.findMany({
+        select: {
+          commentary: true,
+          youtubeEmbed: true,
+          choices: {
+            select: {
+              content: true,
+              rhyme: {
+                select: { content: true },
+              },
             },
-          })
-          if (rhyme) {
-            rhymesKeyContentValueId[rhyme.content] = rhyme.id
-            continue
-          }
-          const createdRhyme = await tx.rhyme.create({
-            data: {
-              content: rhymeWord,
-            },
-          })
-          rhymesKeyContentValueId[createdRhyme.content] = createdRhyme.id
-        }
-
-        const quiz = await tx.quiz.create({
-          data: {
-            userId: userId,
-            commentary: commentary,
-            youtubeEmbed: youtubeEmbed,
           },
-        })
-        console.info(quiz)
+        },
+      })
+      return res.status(200).json(quiz)
+    } catch (error) {
+      console.error(error)
+      return res.status(500).end(error)
+    }
+  }
 
-        for (const choice of choices) {
-          if (choice.rhyme) {
+  if (req.method === 'POST') {
+    const { userId, commentary, youtubeEmbed, choices } = req.body
+    try {
+      await prisma.$transaction(
+        async (tx: Prisma.TransactionClient) => {
+          const rhymeWords: string[] = choices
+            .map((choice: InputChoice) => {
+              if (choice.rhyme) return choice.rhyme
+            })
+            .filter(Boolean)
+
+          let rhymesKeyContentValueId: { [key: string]: string } = {}
+
+          for (const rhymeWord of rhymeWords) {
+            const rhyme = await tx.rhyme.findUnique({
+              where: {
+                content: rhymeWord,
+              },
+            })
+            if (rhyme) {
+              rhymesKeyContentValueId[rhyme.content] = rhyme.id
+              continue
+            }
+            const createdRhyme = await tx.rhyme.create({
+              data: {
+                content: rhymeWord,
+              },
+            })
+            rhymesKeyContentValueId[createdRhyme.content] = createdRhyme.id
+          }
+
+          const quiz = await tx.quiz.create({
+            data: {
+              userId: userId,
+              commentary: commentary,
+              youtubeEmbed: youtubeEmbed,
+            },
+          })
+          console.info(quiz)
+
+          for (const choice of choices) {
+            if (choice.rhyme) {
+              await tx.choice.create({
+                data: {
+                  quizId: quiz.id,
+                  content: choice.content,
+                  rhymeId: rhymesKeyContentValueId[choice.rhyme],
+                },
+              })
+              continue
+            }
             await tx.choice.create({
               data: {
                 quizId: quiz.id,
                 content: choice.content,
-                rhymeId: rhymesKeyContentValueId[choice.rhyme],
               },
             })
-            continue
           }
-          await tx.choice.create({
-            data: {
-              quizId: quiz.id,
-              content: choice.content,
-            },
-          })
+          return res.status(201).json(quiz)
+        },
+        {
+          isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
         }
-        return res.status(201).json(quiz)
-      },
-      {
-        isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
-      }
-    )
-  } catch (error) {
-    console.error(error)
-    return res.status(500).end(error)
+      )
+    } catch (error) {
+      console.error(error)
+      return res.status(500).end(error)
+    }
   }
 }
 
