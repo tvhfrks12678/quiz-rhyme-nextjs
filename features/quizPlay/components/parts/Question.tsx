0fe1ed8e0243b4221b7be97bbaec0f34f003b01a
tvhfrks12678 <tvhfrks12678@gmail.com>
2023-02-07 23:46:26 +0900

    :recycle: front: クイズの問題文用のコンポーネントを追加する

---
+++ features/quizPlay/components/parts/Question.tsx
@@ -0,0 +1,10 @@
+export const Question = () => {
+  return (
+    <div className="flex">
+      <div className="md:text-5xl text-3xl mr-2">Q.</div>
+      <h2 className="md:text-4xl text-2xl flex items-center">
+        韻を踏んでる組み合わせは？
+      </h2>
+    </div>
+  )
+}
